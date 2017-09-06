941f4b9d4a023c0ef1ef53780c57096f32fb86b5
Paul Murff <titan.murff@gmail.com>
2017-09-06 13:16:37 -0600

    suveys almost done

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(passport.session());
 // ROUTES
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
-// require('./routes/surveyRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
